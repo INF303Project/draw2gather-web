7111a562ffcfe355a84010c1a3c9ffb5f22bb59e
Alp Eren Ünal <alpernunl@gmail.com>
2023-12-19 20:56:40 +0300

    deleted dummy stores

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -11,89 +11,6 @@ export type Player = {
     score: number;
 }
 
-// dummy player list
-const player_list = [
-    { name: "alp", score: 0 },
-    { name: "eren", score: 10 },
-    { name: "ünal", score: 20 },
-    { name: "sena", score: 30 },
-    { name: "zeynep", score: 40 },
-    { name: "anıl", score: 50 },
-    { name: "batuhan", score: 60 },
-    { name: "alper", score: 100 },
-    { name: "alp", score: 0 },
-    { name: "eren", score: 10 },
-    { name: "ünal", score: 20 },
-    { name: "sena", score: 30 },
-    { name: "zeynep", score: 40 },
-    { name: "anıl", score: 50 },
-    { name: "batuhan", score: 60 },
-    { name: "alper", score: 100 },
-    { name: "alp", score: 0 },
-    { name: "eren", score: 10 },
-    { name: "ünal", score: 20 },
-    { name: "sena", score: 30 },
-    { name: "zeynep", score: 40 },
-    { name: "anıl", score: 50 },
-    { name: "batuhan", score: 60 },
-    { name: "alper", score: 100 },
-]
-
-// dummy answer list
-const answer_list = [
-    { name: "alp", message: "alp" },
-    { name: "eren", message: "eren" },
-    { name: "ünal", message: "ünal" },
-    { name: "sena", message: "sena" },
-    { name: "zeynep", message: "zeynep" },
-    { name: "anıl", message: "anıl" },
-    { name: "batuhan", message: "batuhan" },
-    { name: "alper", message: "alper" },
-    { name: "alp", message: "alp" },
-    { name: "eren", message: "eren" },
-    { name: "ünal", message: "ünal" },
-    { name: "sena", message: "sena" },
-    { name: "zeynep", message: "zeynep" },
-    { name: "anıl", message: "anıl" },
-    { name: "batuhan", message: "batuhan" },
-    { name: "alper", message: "alper" },
-    { name: "alp", message: "alp" },
-    { name: "eren", message: "eren" },
-    { name: "ünal", message: "ünal" },
-    { name: "sena", message: "sena" },
-    { name: "zeynep", message: "zeynep" },
-    { name: "anıl", message: "anıl" },
-    { name: "batuhan", message: "batuhan" },
-    { name: "alper", message: "alper" },
-]
-
-// dummy message list
-const message_list = [
-    { name: "alp", message: "alp" },
-    { name: "eren", message: "eren" },
-    { name: "ünal", message: "ünal" },
-    { name: "sena", message: "sena" },
-    { name: "zeynep", message: "zeynep" },
-    { name: "anıl", message: "anıl" },
-    { name: "batuhan", message: "batuhan" },
-    { name: "alper", message: "alper" },
-    { name: "alp", message: "alp" },
-    { name: "eren", message: "eren" },
-    { name: "ünal", message: "ünal" },
-    { name: "sena", message: "sena" },
-    { name: "zeynep", message: "zeynep" },
-    { name: "anıl", message: "anıl" },
-    { name: "batuhan", message: "batuhan" },
-    { name: "alper", message: "alper" },
-    { name: "alp", message: "alp" },
-    { name: "eren", message: "eren" },
-    { name: "ünal", message: "ünal" },
-    { name: "sena", message: "sena" },
-    { name: "zeynep", message: "zeynep" },
-    { name: "anıl", message: "anıl" },
-    { name: "batuhan", message: "batuhan" },
-    { name: "alper", message: "alper" },
-]
 
 export const DEFAULT_DRAW_COLOR = "#000000";
 export const DEFAULT_LINE_WIDTH = 4;
@@ -105,6 +22,6 @@ export const boardColor = writable(DEFAULT_DRAW_COLOR);
 export const pencilWidth = writable(DEFAULT_LINE_WIDTH);
 export const eraserWidth = writable(DEFAULT_ERASER_WIDTH);
 
-export const players = writable<Player[]>(player_list);
-export const guesses = writable<Message[]>(answer_list);
-export const messages = writable<Message[]>(message_list);
+export const players = writable<Player[]>([]);
+export const guesses = writable<Message[]>([]);
+export const messages = writable<Message[]>([]);
